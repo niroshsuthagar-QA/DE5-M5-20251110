e759ee73550fa1235731fc6768a55a0e1e021e45
Nirosh Suthagar <nirosh.suthagar@qa.com>
2025-11-11 14:49:08 +0000

    Added Calculator App

---
+++ tests_demo/calculator_app.py
@@ -0,0 +1,18 @@
+class Calculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def get_sum(self):
+        return self.a + self.b
+    def get_difference(self):
+        return self.a - self.b
+    def get_product(self):
+        return self.a * self.b
+    def get_quotient(self):
+        return self.a / self.b
+
+# Instantiates a calculator
+myCalc = Calculator(2,4)
+
+# Applies the method get_product
+print(myCalc.get_product())
