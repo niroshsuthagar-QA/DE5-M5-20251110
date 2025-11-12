f61979df3cc3fea9bf0b1c2514f1210523327dec
Nirosh Suthagar <nirosh.suthagar@qa.com>
2025-11-12 15:44:58 +0000

    Docker Demo with dependencies.

--- docker_demo/level_1_demo.py
+++ docker_demo/level_1_demo.py
@@ -1,1 +1,10 @@
-print("Hello world, I just used docker!")
+import pandas as pd
+
+print("Hello world, I just used docker for the second time!")
+
+df = pd.DataFrame({
+    'name': ['Nirosh', 'Bouchra'],
+    'eyeCol': ["Brown", "Blue"]
+})
+
+print(df)
